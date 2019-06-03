5da78b914993c829e301ba88d06c3e1b47fce8ec
Lusjen <lusjenh@gmail.com>
2019-06-03 15:45:59 +0300

    first commit

---
+++ dist/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">	
+	<link rel="stylesheet" href="css/main.min.css">
+</head>
+<body>
+	<!-- begin header -->
+	<header class="header">
+		<div class="container">
+			<div class="header__wrap">
+				<div class="logo">
+					<img src="img/EG_logo_eng.svg" alt="logo">
+				</div>
+				<ul class="menu">
+					<li class="menu__item">
+						<a href="#" class="menu__link">Проектування</a>
+					</li>
+					<li class="menu__item">
+						<a href="#" class="menu__link">Системи Автоматичного поливу</a>
+					</li>
+					<li class="menu__item">
+						<a href="#" class="menu__link">Рулонний газон</a>
+					</li>
+					<li class="menu__item">
+						<a href="#" class="menu__link">Послуги</a>
+					</li>
+					<li class="menu__item">
+						<a href="#" class="menu__link">Відео-Фото</a>
+					</li>
+					<li class="menu__item">
+						<a href="#" class="menu__link">Контакти</a>
+					</li>
+				</ul>
+				<div class="tel">
+					<a href="tel:+8067 327-12-87" class="tel__link">(067) 327-12-87</a>
+				</div>
+				<div class="lang">
+					<a href="" class="lang__item">Рус</a>
+					<a href="" class="lang__item">Укр</a>
+				</div>
+			</div>		
+		</div>		
+	</header>
+	<!-- end header -->
+	<!-- begin one -->
+	<div class="one">
+		<div class="slider-one js-slider-one">
+			<div class="slider-one__item">
+				<img src="img/bg-1.png" alt="">
+			</div>
+			<div class="slider-one__item">
+				<img src="img/bg-1.png" alt="">
+			</div>
+			<div class="slider-one__item">
+				<img src="img/bg-1.png" alt="">
+			</div>
+			<div class="slider-one__item">
+				<img src="img/bg-1.png" alt="">
+			</div>
+		</div>
+		<div class="slider-one__info">
+			<div class="container">
+				<div class="slider-one__info-in">
+					<h1 class="title-lg">
+						Прохолода і бездоганність натуральних газонів
+					</h1>
+					<p class="slider-one__text">
+						<span class="bold">Компанія EVROGAZON</span><br> 
+						працює для Вашого задоволення.
+					</p>
+					<div class="slider-one__btns">
+						<div class="slider-one__btn slider-one__btn-prev">
+							<img src="img/Arrow.svg" alt="">
+						</div>
+						<div class="slider-one__btn slider-one__btn-next">
+							<img src="img/Arrow.svg" alt="">
+						</div>
+					</div>
+				</div>		
+			</div>	
+		</div>
+	</div>
+	<!-- end one -->
+	
+	<script src="js/scripts.min.js"></script>
+	<script src=""></script>
+	
+</body>
+</html>
